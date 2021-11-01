9b613dd096e2a236d607b943c6e3f468e5463276
gustave-studio <gustave0222@gmail.com>
2021-11-01 15:55:33 +0900

    ✨トップページの調整

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -7,7 +7,7 @@ import Typography from '@mui/material/Typography';
 import StartButton from './StartButton';
 
 const Top = () => {
-  const imgUrl = 'https://ra-test-images.s3.ap-northeast-1.amazonaws.com/sample.png';
+  const imgUrl = 'https://no-longer-human.s3.ap-northeast-1.amazonaws.com/result1.png';
 
   return (
     <>
@@ -29,7 +29,9 @@ const Top = () => {
       </HelmetProvider>
       <div className="container">
         <h1>
-          「人間失格度」診断
+          「人間失格度」
+          <br />
+          診断
         </h1>
 
         <div className="explanation">
@@ -40,7 +42,9 @@ const Top = () => {
                   太宰治の「人間失格」に、どの位あなたの性格が一致しているかを診断します。
                   <br />
                   <br />
-                  所用時間1分
+                  全4問
+                  <br />
+                  所用時間30秒
                 </div>
               </Typography>
             </CardContent>
