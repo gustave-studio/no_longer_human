9c3fe0c723048554191f832e72c3739f495f8e90
gustave-studio <gustave0222@gmail.com>
2021-11-01 15:32:17 +0900

    ✨resultページ調整

--- src/components/ResultPage.jsx
+++ src/components/ResultPage.jsx
@@ -12,6 +12,10 @@ import {
   LineIcon,
 } from 'react-share';
 import PropTypes from 'prop-types';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Grid from '@material-ui/core/Grid';
+import { Link } from 'react-router-dom';
 import Results from '../Results';
 
 const ResultPage = (props) => {
@@ -37,29 +41,52 @@ const ResultPage = (props) => {
       <div className="container">
         <h1>{Results[result].title}</h1>
         <h2>{Results[result].class}</h2>
-        <Card sx={{ minWidth: 275 }}>
-          <CardContent>
-            <Typography variant="h5" component="div">
-              <div>{Results[result].containts}</div>
-            </Typography>
-          </CardContent>
-          <FacebookShareButton
-            url={`http://localhost:3000/${result}`}
-            quote={['']}
-          >
-            <FacebookIcon size={50} round />
-          </FacebookShareButton>
-          <TwitterShareButton
-            url={`http://localhost:3000/${result}`}
-            title={['']}
-          >
-            <TwitterIcon size={50} round />
-          </TwitterShareButton>
-          <LineShareButton url={`http://localhost:3000/${result}`}>
-            <LineIcon size={50} round />
-          </LineShareButton>
-        </Card>
+        <div className="explanation">
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                <div>{Results[result].containts}</div>
+              </Typography>
+            </CardContent>
+            <FacebookShareButton
+              url={`http://localhost:3000/${result}`}
+              quote={['']}
+            >
+              <FacebookIcon size={50} round />
+            </FacebookShareButton>
+            <TwitterShareButton
+              url={`http://localhost:3000/${result}`}
+              title={['']}
+            >
+              <TwitterIcon size={50} round />
+            </TwitterShareButton>
+            <LineShareButton url={`http://localhost:3000/${result}`}>
+              <LineIcon size={50} round />
+            </LineShareButton>
+          </Card>
+        </div>
       </div>
+      <br />
+      <Grid container>
+        <Grid item xs={3} />
+        <Grid item xs={6}>
+          <div>
+            <Stack spacing={2} direction="row">
+              <Grid item container direction="column" spacing={0}>
+                <Button
+                  style={{ backgroundColor: '#ff1493', fontSize: '20px' }}
+                  variant="contained"
+                  component={Link}
+                  to="/"
+                >
+                  診断トップへ
+                </Button>
+              </Grid>
+            </Stack>
+          </div>
+        </Grid>
+        <Grid item xs={3} />
+      </Grid>
     </>
   );
 };
