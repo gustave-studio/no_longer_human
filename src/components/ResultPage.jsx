45e28338c7849218e0025752adf625854f433ac7
gustave-studio <gustave0222@gmail.com>
2021-11-12 10:18:32 +0900

    ✨広告リンク追加

--- src/components/ResultPage.jsx
+++ src/components/ResultPage.jsx
@@ -26,6 +26,36 @@ const ResultPage = (props) => {
     </div>
   ));
 
+  const advertisement = () => {
+    switch (result) {
+      case 'result1':
+        return (
+          <iframe title="advertisement" className="advertisement" style={{ width: '120px', height: '240px' }} marginWidth="0" marginHeight="0" scrolling="no" frameBorder="0" src="https://rcm-fe.amazon-adsystem.com/e/cm?ref=tf_til&t=gustave02-22&m=amazon&o=9&p=8&l=as1&IS1=1&detail=1&asins=4101006067&linkId=5a0dc653dc9ac7961c4bffa2e4408e2b&bc1=ffffff&amp;lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr" />
+        );
+      case 'result2':
+        return (
+          <iframe title="advertisement" className="advertisement" style={{ width: '120px', height: '240px' }} marginWidth="0" marginHeight="0" scrolling="no" frameBorder="0" src="https://rcm-fe.amazon-adsystem.com/e/cm?ref=tf_til&t=gustave02-22&m=amazon&o=9&p=8&l=as1&IS1=1&detail=1&asins=4101006075&linkId=1ba564822a40f9269233aebc00f29fca&bc1=ffffff&amp;lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr" />
+        );
+      case 'result3':
+        return (
+          <iframe title="advertisement" className="advertisement" style={{ width: '120px', height: '240px' }} marginWidth="0" marginHeight="0" scrolling="no" frameBorder="0" src="https://rcm-fe.amazon-adsystem.com/e/cm?ref=tf_til&t=gustave02-22&m=amazon&o=9&p=8&l=as1&IS1=1&detail=1&asins=4041099153&linkId=11a553ab9d354496396890f0be5c4a45&bc1=ffffff&amp;lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr" />
+        );
+
+      case 'result4':
+        return (
+          <iframe title="advertisement" className="advertisement" style={{ width: '120px', height: '240px' }} marginWidth="0" marginHeight="0" scrolling="no" frameBorder="0" src="https://rcm-fe.amazon-adsystem.com/e/cm?ref=tf_til&t=gustave02-22&m=amazon&o=9&p=8&l=as1&IS1=1&detail=1&asins=4101006032&linkId=f77b9d132dafbc06dbd20161a13a0c12&bc1=ffffff&amp;lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr" />
+        );
+      case 'result5':
+        return (
+          <iframe title="advertisement" className="advertisement" style={{ width: '120px', height: '240px' }} marginWidth="0" marginHeight="0" scrolling="no" frameBorder="0" src="https://rcm-fe.amazon-adsystem.com/e/cm?ref=tf_til&t=gustave02-22&m=amazon&o=9&p=8&l=as1&IS1=1&detail=1&asins=B00FA4GG5E&linkId=16ddc97185f1683560f2e111a6901998&bc1=ffffff&amp;lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr" />
+        );
+      default:
+        return (
+          <iframe title="advertisement" className="advertisement" style={{ width: '120px', height: '240px' }} marginWidth="0" marginHeight="0" scrolling="no" frameBorder="0" src="https://rcm-fe.amazon-adsystem.com/e/cm?ref=tf_til&t=gustave02-22&m=amazon&o=9&p=8&l=as1&IS1=1&detail=1&asins=B00FA4GG5E&linkId=16ddc97185f1683560f2e111a6901998&bc1=ffffff&amp;lt1=_blank&fc1=333333&lc1=0066c0&bg1=ffffff&f=ifr" />
+        );
+    }
+  };
+
   return (
     <>
       <HelmetProvider>
@@ -50,22 +80,27 @@ const ResultPage = (props) => {
         <div className="explanation">
           <Card sx={{ minWidth: 275 }}>
             <CardContent>
-              <Typography variant="h5" component="div">
+              <Typography
+                variant="string"
+                component="div"
+                style={{ fontSize: '18px' }}
+              >
                 {texts}
               </Typography>
             </CardContent>
-            <FacebookShareButton
-              url={`https://no-longer-human.gustave-studio.com/${result}`}
-              quote={['']}
-            >
-              <FacebookIcon size={50} round />
-            </FacebookShareButton>
+            { advertisement() }
             <TwitterShareButton
               url={`https://no-longer-human.gustave-studio.com/${result}`}
               title={['']}
             >
               <TwitterIcon size={50} round />
             </TwitterShareButton>
+            <FacebookShareButton
+              url={`https://no-longer-human.gustave-studio.com/${result}`}
+              quote={['']}
+            >
+              <FacebookIcon size={50} round />
+            </FacebookShareButton>
             <LineShareButton url={`https://no-longer-human.gustave-studio.com/${result}`}>
               <LineIcon size={50} round />
             </LineShareButton>
